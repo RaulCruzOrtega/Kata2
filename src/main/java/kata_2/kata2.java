4c90f9e18edf76fa03f53fcf3f66a986afe644f2
rauli <rauli@LAPTOP-ALE41ACI>
2022-09-30 22:02:44 +0100

    Kata2 versión4

--- src/main/java/kata_2/kata2.java
+++ src/main/java/kata_2/kata2.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class kata2 {
 
     public static void main(String[] args) {
-        int [] data = {1,6,2,8,3,8,2,7,3,4,2,9,0,6,0};
+        Integer [] data = {1,6,2,8,3,8,2,7,3,4,2,9,0,6,0};
         Histogram histo = new Histogram(data);
         
         Map<Integer, Integer> histogr = histo.getHistogram();
